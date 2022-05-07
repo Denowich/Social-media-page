ad699a5c4649a182e5b08cefa885b0afbb11eaec
Den denowich <denowich@gmail.com>
2022-05-07 20:39:07 +0300

    added links for users in page

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import style from "./Dialogs.module.css";
 
 
@@ -7,22 +8,22 @@ const Dialogs = () => {
         <div className={style.dialogs}>
             <div className={style.dialogsItems}>
                 <div className={style.dialog}>
-                    Valera
+                    <NavLink to= '/dialogs/1'>Valera</NavLink>
                 </div>
                 <div className={style.dialog + ' ' + style.active}>
-                    Dimych
+                    <NavLink to= '/dialogs/2'Dimych></NavLink>
                 </div>
                 <div className={style.dialog}>
-                    Andrey
+                    <NavLink to= '/dialogs/3'>Andrey</NavLink>
                 </div>
                 <div className={style.dialog}>
-                    Roman
+                    <NavLink to= '/dialogs/4'>Roman</NavLink>
                 </div>
                 <div className={style.dialog}>
-                    Svetlana
+                    <NavLink to= '/dialogs/5'>Svetlana</NavLink>
                 </div>
                 <div className={style.dialog}>
-                    Victor
+                    <NavLink to= '/dialogs/6'>Victor</NavLink>
                 </div>
             </div>
                 <div className={style.messages}>
