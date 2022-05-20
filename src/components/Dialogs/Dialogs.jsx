63c237e537d4e68feb166262189dd1094f594dd0
Den denowich <denowich@gmail.com>
2022-05-20 15:00:17 +0300

    add todoItems

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,47 +1,32 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
 import style from "./Dialogs.module.css";
+import DialogItem from "./DialogItem/DialogItem";
+import Message from "./Message/Message";
 
-
-const DialogItem = (props) => {
-
-    let path = '/dialogs/' + props.id;
-        return (
-            <div className={style.dialog + ' ' + style.active}>
-                <NavLink to= {path}> {props.name} </NavLink>
-            </div>
-    );
-}
-
-const Message = (props) => {
-    return (
-        <div className={style.dialog}>{props.message}</div>
-    );
-}
 
 const Dialogs = () => {
 
     let dialogs = [
-        {id: 1, name: 'Dimych'},
-        {id: 2, name: 'Andrey'},
-        {id: 3, name: 'Sveta'},
-        {id: 4, name: 'Sasha'},
-        {id: 5, name: 'Victor'},
-        {id: 6, name: 'Valera'}
+        { id: 1, name: 'Dimych' },
+        { id: 2, name: 'Andrey' },
+        { id: 3, name: 'Sveta' },
+        { id: 4, name: 'Sasha' },
+        { id: 5, name: 'Victor' },
+        { id: 6, name: 'Valera' }
     ]
 
     let messages = [
-        {id: 1, message: 'Hi!'},
-        {id: 2, message: 'How are you?'},
-        {id: 3, message: 'Where are you from?'},
-        {id: 4, message: 'Yo!'},
-        {id: 5, message: 'Yo!'},
-        {id: 6, message: 'Yo!'}
+        { id: 1, message: 'Hi!' },
+        { id: 2, message: 'How are you?' },
+        { id: 3, message: 'Where are you from?' },
+        { id: 4, message: 'Yo!' },
+        { id: 5, message: 'Yo!' },
+        { id: 6, message: 'Yo!' }
     ]
 
-    
-    let dialogsElements = dialogs.map( (d) => <DialogItem name={d.name} id={d.id}/>);
-    let messegesElements = messages.map((m)=><Message message={m.message}/>);
+
+    let dialogsElements = dialogs.map((d) => <DialogItem name={d.name} id={d.id} />);
+    let messegesElements = messages.map((m) => <Message message={m.message} />);
 
     return (
         <div className={style.dialogs}>
